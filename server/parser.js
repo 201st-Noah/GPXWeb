01e43ffd038347882cd6df9f5d6ac8d2941a3d50
noah <noahbackes2@gmail.com>
2023-12-01 13:44:53 +0100

    paser: all basic funktions

--- server/parser.js
+++ server/parser.js
@@ -21,7 +21,7 @@ function getTrackPoints(filePath, callback) {
     });
   });
 }
-
+//distance
 function getLength(file, callback) {
   getTrackPoints(file, (err, trackPoints) => {
     if (err) {
@@ -34,10 +34,10 @@ function getLength(file, callback) {
       const t1 = new Date(trackPoints[i - 1].time).getTime()
       const t2 = new Date(trackPoints[i].time).getTime()
       if((t2 - t1) < 10000) {
-        const lat1 = parseFloat(trackPoints[i - 1].$.lat);
-        const lon1 = parseFloat(trackPoints[i - 1].$.lon);
-        const lat2 = parseFloat(trackPoints[i].$.lat);
-        const lon2 = parseFloat(trackPoints[i].$.lon);
+        const lat1 = parseFloat(trackPoints[i - 1].$.lat).toFixed(8);
+        const lon1 = parseFloat(trackPoints[i - 1].$.lon).toFixed(8);
+        const lat2 = parseFloat(trackPoints[i].$.lat).toFixed(8);
+        const lon2 = parseFloat(trackPoints[i].$.lon).toFixed(8);
 
         const dLat = (lat2 - lat1) * (Math.PI / 180);
         const dLon = (lon2 - lon1) * (Math.PI / 180);
@@ -50,10 +50,10 @@ function getLength(file, callback) {
         totalDistance += distance;
       }
     }
-    callback(null, totalDistance);
+    callback(null, totalDistance.toFixed(2));
   });
 }
-
+//Altitude
 function getAscent(file, callback){
   getTrackPoints(file, (err, trackPoints) => {
     if (err) {
@@ -75,10 +75,33 @@ function getAscent(file, callback){
         }
       }
     }
-    callback(null, totalAscent.toNumber());
+    callback(null, totalAscent.toNumber().toFixed(0));
   });
 }
+function getDescent(file, callback){
+  getTrackPoints(file, (err, trackPoints) => {
+    if (err) {
+      return callback(err, null);
+    }
 
+    // Calculate total Ascent
+    let totalAscent = new Decimal(0);
+    for (let i = 1; i < trackPoints.length; i++) {
+      const t1 = new Date(trackPoints[i - 1].time).getTime()
+      const t2 = new Date(trackPoints[i].time).getTime()
+      if((t2 - t1) < 10000) {
+        const h1 = new Decimal(trackPoints[i - 1].ele);
+        const h2 = new Decimal(trackPoints[i].ele);
+
+        if (h2 < h1) {
+          const elevDiff = h1.minus(h2)
+          totalAscent = totalAscent.plus(elevDiff)
+        }
+      }
+    }
+    callback(null, totalAscent.toNumber().toFixed(0));
+  });
+}
 function getMaxAlt(file, callback){
   getTrackPoints(file, (err, trackPoints) => {
     if (err) {
@@ -105,6 +128,7 @@ function getMinAlt(file, callback){
     callback(null, minElevation.toFixed(0));
   });
 }
+//time
 function getTotalDuration(file, callback){
   getTrackPoints(file, (err, trackPoints) => {
     if (err) {
@@ -119,7 +143,6 @@ function getTotalDuration(file, callback){
     callback(null, duration)
   });
 }
-
 function getDuration(file, callback){
   getTrackPoints(file, (err, trackPoints) => {
     if (err) {
@@ -139,20 +162,124 @@ function getDuration(file, callback){
     callback(null, totalDuration/1000);
   });
 }
+//speed
+function getMinProKm(file, callback){
+  getLength(file, (err, length) => {
+    if (err) {
+      return callback(err, null);
+    }
+    getDuration(file, (err, duration) => {
+      if (err) {
+        return callback(err, null);
+      }
+      const speed = (duration/60) / (length/1000)
+      callback(null, speed.toFixed(2));
+    });
+  });
+}
+function getKmProH(file, callback){
+  getLength(file, (err, length) => {
+    if (err) {
+      return callback(err, null);
+    }
+    getDuration(file, (err, duration) => {
+      if (err) {
+        return callback(err, null);
+      }
+      const speed = (length/1000) / (duration/3600)
+      callback(null, speed.toFixed(2));
+    });
+  });
+}
+//heart rate
+function getMeanHr(file, callback){
+  getTrackPoints(file, (err, trackPoints) => {
+    if (err) {
+      return callback(err, null);
+    }
+    let meanHf = 0;
+    if(trackPoints[0].extensions != null){
+      for (let i = 0; i < trackPoints.length; i++){
+        meanHf = meanHf + parseInt(trackPoints[i].extensions['ns3:TrackPointExtension']['ns3:hr'])
+      }
+    }
+    callback(null, (meanHf/trackPoints.length).toFixed(0));
+  });
+}
+function getMaxHr(file, callback){
+  getTrackPoints(file, (err, trackPoints) => {
+    if (err) {
+      return callback(err, null);
+    }
+    let maxHf = 0;
+    if(trackPoints[0].extensions != null){
+      for (let i = 0; i < trackPoints.length; i++){
+        maxHf = Math.max(maxHf, parseFloat(trackPoints[i].extensions['ns3:TrackPointExtension']['ns3:hr']))
+      }
+    }
+    callback(null, maxHf.toFixed(0));
+  });
+}
+//start
+function getStartTime(file, callback){
+  getTrackPoints(file, (err, trackPoints) => {
+    if (err) {
+      return callback(err, null);
+    }
+    callback(null, trackPoints[0].time)
+  });
+}
+function getStartCoords(file, callback){
+  getTrackPoints(file, (err, trackPoints) => {
+    if (err) {
+      return callback(err, null);
+    }
+    callback(null, [parseFloat(trackPoints[0].$.lat).toFixed(6), parseFloat(trackPoints[0].$.lon).toFixed(6)])
+  });
+}
+function getType(file, callback){
+  fs.readFile(file, 'utf-8', (err, data) => {
+    if (err) {
+      return callback(err, null);
+    }
+    xml2js.parseString(data, { explicitArray: false }, (parseErr, result) => {
+      if (parseErr) {
+        return callback(parseErr, null);
+      }
+      let res = result.gpx.trk;
+      if(res.type != null){
+        res = res.type
+      }
+      else{
+        res = res.name
+      }
+      callback(null, res.toLowerCase());
+    });
+  });
+}
+
+
 
 //Output
 getLength(filePath, (err, distance) => {
   if (err) {
     console.error('Error:', err);
   } else {
-    console.log('Total Distance:', distance.toFixed(2), 'meters');
+    console.log('Total Distance:', distance, 'meters');
   }
 });
 getAscent(filePath, (err, ascent) => {
   if (err) {
     console.error('Error:', err);
   } else {
-    console.log('Total Ascent:', ascent.toFixed(2), 'meters');
+    console.log('Total Ascent:', ascent, 'meters');
+  }
+});
+getDescent(filePath, (err, ascent) => {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log('Total Descent:', ascent, 'meters');
   }
 });
 getTotalDuration(filePath, (err, duration) => {
@@ -183,4 +310,52 @@ getMinAlt(filePath, (err, duration) => {
     console.log('Lowest Point at:', duration, 'm');
   }
 });
-
+getMinProKm(filePath, (err, duration) => {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log('Speed:', duration, 'min/km');
+  }
+});
+getKmProH(filePath, (err, duration) => {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log('Speed:', duration, 'Km/h');
+  }
+});
+getStartTime(filePath, (err, duration) => {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log('Started at:', duration);
+  }
+});
+getStartCoords(filePath, (err, duration) => {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log('Started at Coords:', duration);
+  }
+});
+getType(filePath, (err, type) =>{
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log('Type:', type);
+  }
+});
+getMeanHr(filePath, (err, type) =>{
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log('Average Heart rate:', type, 'bpm');
+  }
+});
+getMaxHr(filePath, (err, type) =>{
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log('Max Heart rate:', type, 'bpm');
+  }
+});
